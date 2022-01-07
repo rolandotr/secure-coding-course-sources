4a94592c41a426f7cbb10c097cf1c2b02bdcd855
Rolando Trujillo <rolando.trujillo@gmail.com>
2022-01-07 18:24:34 +1100

    Setting up our Spring MVC app without xml configuration files, just using Java code

---
+++ 11-examgeneration/src/main/java/edu/deakin/sit218/examgeneration/config/ExamGenerationAppConfig.java
@@ -0,0 +1,24 @@
+package edu.deakin.sit218.examgeneration.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "edu.deakin.sit218.examgeneration")
+public class ExamGenerationAppConfig {
+
+	// Define a bean for the View Resolver 
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/view/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+	
+}
---
+++ 11-examgeneration/src/main/java/edu/deakin/sit218/examgeneration/controller/HomeController.java
@@ -0,0 +1,13 @@
+package edu.deakin.sit218.examgeneration.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String showHome() {
+		return "home";
+	}
+}
